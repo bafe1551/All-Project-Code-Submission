0b1243cdd7c111765ae8136c9b41c7e57e37fc66
naifalassaf <59854043+naifalassaf@users.noreply.github.com>
2020-03-18 09:49:44 -0600

    combat system stuff

---
+++ project fightr - combat new/Assets/battle_hud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class battle_hud : MonoBehaviour
+{
+
+    public Slider hpSlider;
+
+    public void SetHUD(Unit unit)
+    {
+        hpSlider.maxValue = unit.maxHP;
+        hpSlider.value = unit.currentHP;
+    }
+
+    public void SetHP(int hp)
+    {
+        hpSlider.value = hp;
+    }
+
+}
---
+++ project fightr - combat new/Assets/battle_system.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState{ START, PLAYERTURN, ENEMYTURN, WIN, LOSS}
+
+public class battle_system : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public Text dialogueText;
+
+    public battle_hud enemyHUD;
+    public battle_hud playerHUD;
+
+
+    public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+      GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+      playerUnit = playerGO.GetComponent<Unit>();
+
+      GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+      enemyUnit = enemyGO.GetComponent<Unit>();
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+        yield break;
+    }
+    
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+
+        yield return new WaitForSeconds(2f);
+        
+        if (isDead)
+        {
+            state = BattleState.WIN;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOSS;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WIN)
+        {
+            dialogueText.text = "You won the battle!";
+        }
+        else if (state == BattleState.LOSS)
+        {
+            dialogueText.text = "You were defeated.";
+        }
+    }
+
+    void PlayerTurn()
+    {
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+}
